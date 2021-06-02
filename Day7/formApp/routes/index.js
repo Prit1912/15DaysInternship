59c46b60daf02a84d78424cb1c23c51b976e7a7c
Prit Rojivadiya <pritrojivadiya@gmail.com>
2021-06-02 19:52:58 +0530

    Add files via upload

---
+++ Day7/formApp/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+router.post('/form_process', function(req, res, next) {
+  fname = req.body.fname;
+  mname = req.body.mname;
+  lname = req.body.lname;
+  cname = req.body.cname;
+  sem = req.body.sem;
+  phone = req.body.phone;
+  mail = req.body.mail;
+  uname = req.body.uname;
+  pwd = req.body.pwd;
+  pwd1 = req.body.pwd1;
+  res.render('formOutput',{fname:fname,lname:lname,cname:cname,sem:sem,phone:phone,mail:mail,uname:uname,pwd:pwd,pwd1:pwd1});
+});
+
+module.exports = router;
